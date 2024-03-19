1f80036101a40630f97bc317ec27d751aeae4b9b
harry <gavriilidis@tu-berlin.de>
2024-03-19 20:41:43 +0100

    introduced compile flags for linux

--- setup.py
+++ setup.py
@@ -2,6 +2,17 @@ from setuptools import setup, Extension
 import numpy
 from numpy.distutils.misc_util import Configuration, get_info
 from numpy.distutils.core import setup
+import platform
+
+extra_compile_args = []
+extra_link_args = []
+
+if platform.system() == 'Windows':
+    extra_compile_args = ["/std:c++17", "/W4", "/DTARGET_PYTHON"]
+    extra_link_args = []
+elif platform.system() == 'Linux':
+    extra_compile_args = ["-std=c++17", "-W4", "-DTARGET_PYTHON"]
+    extra_link_args = []
 
 sheetreader = Extension(
     "sheetreader",
@@ -11,6 +22,9 @@ sheetreader = Extension(
         "src/sheetreader-core/src/XlsxFile.cpp",
         "src/sheetreader-core/src/miniz/miniz.cpp",
     ],
-    extra_compile_args=["/std:c++17", "/W4", "/DTARGET_PYTHON"])#, "/DEBUG:FULL"], extra_link_args=["/DEBUG:FULL"])
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args
+)
 
 setup(name="SheetReader", ext_modules=[sheetreader], options={"build": {"build_lib": "."}})
+
